5d8591871d55a6ea5ec9c954bc6432177a7bc5da
sagar dhankar <92321286+sagaroshiv@users.noreply.github.com>
2023-03-14 22:30:58 +0530

    Write to a file.

--- file.py
+++ file.py
@@ -1,10 +1,8 @@
-file = open("demo.txt", 'r')                                                                                            # read only
-
-line = file.readline()                                                                                                  # line is a String
-while line:
-    print("Line read- " + line)
-    line = file.readline()                                                                                              # in a readline() , String concatenated with "\n". And file content First line is deleted.
-# file is IOWrapper , so we need to convert it into String => file.read() to convert into String
-
-# make sure to close the file
+file = open("demo.txt", 'w')
+file.write("this is content for the file")                                                                              # this overwrite the text over last text.
 file.close()
+
+file = open("demo.txt", 'r')
+content = file.read()
+print(content)
+
