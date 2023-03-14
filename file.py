cecc3aa3bb53761a7e53c123f47afe8bb3636820
sagar dhankar <92321286+sagaroshiv@users.noreply.github.com>
2023-03-14 22:21:38 +0530

    Read File - line by line.

--- file.py
+++ file.py
@@ -1,7 +1,10 @@
 file = open("demo.txt", 'r')                                                                                            # read only
 
-content = file.readline()
-print(content)
+line = file.readline()                                                                                                  # line is a String
+while line:
+    print("Line read- " + line)
+    line = file.readline()                                                                                              # in a readline() , String concatenated with "\n". And file content First line is deleted.
+# file is IOWrapper , so we need to convert it into String => file.read() to convert into String
 
 # make sure to close the file
 file.close()
