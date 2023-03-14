2ed742080297135bca32a12250f4585e55388886
sagar dhankar <92321286+sagaroshiv@users.noreply.github.com>
2023-03-14 22:41:18 +0530

    Adding more text without overwriting.

--- file.py
+++ file.py
@@ -1,5 +1,5 @@
 file = open("demo.txt", 'w')                                                                                            # we have to specify in which mode we have to open the file
-file.write("This is the content for the file.")
+file.write("This is the content for the file. ")
 file.close()
 
 file = open("demo.txt", 'r')                                                                                            #opening file in Read Mode
@@ -7,8 +7,8 @@ content = file.read()
 print(content)
 file.close()
 
-file = open("demo.txt", 'w')
-file.write("this is the new content.")                                                                                  #This writing OverWrite the content over last saved content.
+file = open("demo.txt", 'a')
+file.write("This is the new content.")                                                                                  #It donot interfere with the last saved content.
 file.close()
 
 file = open("demo.txt", 'r')
