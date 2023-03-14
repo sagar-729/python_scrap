d0ab89551a5bceab44003fd8f18f29829d7b92ae
sagar dhankar <92321286+sagaroshiv@users.noreply.github.com>
2023-03-14 22:38:02 +0530

    writing repeatedily - overwrites the content.

--- file.py
+++ file.py
@@ -1,8 +1,17 @@
+file = open("demo.txt", 'w')                                                                                            # we have to specify in which mode we have to open the file
+file.write("This is the content for the file.")
+file.close()
+
+file = open("demo.txt", 'r')                                                                                            #opening file in Read Mode
+content = file.read()
+print(content)
+file.close()
+
 file = open("demo.txt", 'w')
-file.write("this is content for the file")                                                                              # this overwrite the text over last text.
+file.write("this is the new content.")                                                                                  #This writing OverWrite the content over last saved content.
 file.close()
 
 file = open("demo.txt", 'r')
 content = file.read()
 print(content)
-
+file.close()
