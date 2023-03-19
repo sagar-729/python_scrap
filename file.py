d8ff6b5cacc22ba4f54d0e268ed71d33b758171f
sagar dhankar <92321286+sagaroshiv@users.noreply.github.com>
2023-03-19 07:54:56 +0530

    Dictionary -
    Access of key using []

--- file.py
+++ file.py
@@ -1,1 +1,21 @@
 # creating infrastructure for our new branch project
+# creating infrastructure for our new branch project
+
+numbers = {
+    1 : "One",
+    2 : "Two",
+    3 : "Three",
+    4 : "Four"
+}
+
+print(numbers[5])                                                                                                       # Error :- Get keyerror at this line . Code below this line donot execute.
+
+people = {
+    "John" : 21,
+    "Shyam" : 8,
+    "Ram" : 27
+}
+
+name = "Ram"
+
+print("Age of " + name + " is "+  str(people[name]))
